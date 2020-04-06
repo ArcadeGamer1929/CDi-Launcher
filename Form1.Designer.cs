49ff60b4d3681dc4fb52700f49663b5072cf05d7
Arcade Gamer <49656813+ArcadeGamer1929@users.noreply.github.com>
2020-04-06 09:48:53 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace CDi_Launcher
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.discordRichPresenceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.discordRichPresenceToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.FileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // discordRichPresenceToolStripMenuItem
+            // 
+            this.discordRichPresenceToolStripMenuItem.BackColor = System.Drawing.SystemColors.Control;
+            this.discordRichPresenceToolStripMenuItem.Checked = true;
+            this.discordRichPresenceToolStripMenuItem.CheckOnClick = true;
+            this.discordRichPresenceToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.discordRichPresenceToolStripMenuItem.Name = "discordRichPresenceToolStripMenuItem";
+            this.discordRichPresenceToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.discordRichPresenceToolStripMenuItem.Text = "Discord Rich Presence";
+            this.discordRichPresenceToolStripMenuItem.CheckedChanged += new System.EventHandler(this.DiscordRichPresenceToolStripMenuItem_CheckedChanged);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(704, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = global::CDi_Launcher.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(704, 441);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "CDi Launcher";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainWindow_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripMenuItem discordRichPresenceToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using DiscordRPC;
+
+namespace CDi_Launcher
+{
+    public partial class MainWindow : Form
+    {
+        public DiscordRpcClient client;
+        Process process = new Process();
+        int seconds = 0;
+        TimeSpan time;
+        string game = "Idle";
+        string timeToString;
+        bool disablingRPC = false;
+        string status = "Idle";
+
+        private int CDiBiosChecker()
+        {
+            if (File.Exists("setup.txt"))
+            {
+                string readText = System.IO.File.ReadAllText("setup.txt");
+                if (readText.Substring(readText.Length - 3) == "Q==")
+                {
+                    if (File.Exists("roms/cdimono1.zip") && File.Exists("roms/cdimono1/cdibios.zip"))
+                    {
+                        return 1;
+                    } else
+                    {
+                        var yes = System.Convert.FromBase64String(readText);
+                        JObject o = JObject.Parse(System.Text.Encoding.UTF8.GetString(yes));
+                        // o["download1"].ToString();
+                        WebClient web = new WebClient();
+                        Directory.CreateDirectory("roms");
+                        web.DownloadFile(o["download1"].ToString(), "roms/cdimono1.zip");
+                        Console.WriteLine(o["download1"].ToString());
+                        Directory.CreateDirectory("roms/cdimono1");
+                        web.DownloadFile(o["download2"].ToString(), "roms/cdimono1/cdibios.zip");
+                        Console.WriteLine(o["download2"].ToString());
+                        return 1;
+                    }
+                } else {
+                    System.Windows.Forms.MessageBox.Show("In setup.txt, please add \"Q==\" (without quotes, and in capitals) at the end of the file.");
+                }
+            } else {
+                using (StreamWriter sw = File.AppendText("setup.txt"))
+                { // Q==
+                    sw.Write("e2Rvd25sb2FkMToiaHR0cHM6Ly9hcmNoaXZlLm9yZy9kb3dubG9hZC9NYW1lMTcyYXJjZGNtcGx0L2NkaW1vbm8xLnppcCIsZG93bmxvYWQyOiJodHRwczovL2FyY2hpdmUub3JnL2Rvd25sb2FkL01hbWUxNzJhcmNkY21wbHQvY2RpYmlvcy56aXAif");
+                    System.Windows.Forms.MessageBox.Show("In setup.txt, please add \"Q==\" (without quotes, and in capitals) at the end of the file.");
+                    return 0;
+                    // new Setup().Show();
+                }
+            }
+            return 0;
+        }
+
+        void RPCinit()
+        {
+            client = new DiscordRpcClient("696311197024256060");
+            timeToString = time.ToString(@"mm\:ss");
+            client.OnReady += (sender, e) =>
+            {
+                Console.WriteLine("Received Ready from user {0}", e.User.Username);
+            };
+            client.OnPresenceUpdate += (sender, e) =>
+            {
+                // Console.WriteLine("Received Update! {0}", e.Presence);
+            };
+            client.Initialize();
+            client.SetPresence(new RichPresence()
+            {
+                Details = game,
+                State = status,
+                Assets = new Assets()
+                {
+                    LargeImageKey = "rpc-icon"
+                }
+            });
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CDiBiosChecker();
+            RPCinit();
+        }
+
+        private void FileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (CDiBiosChecker() == 1)
+            {
+                Process[] pname = Process.GetProcessesByName("mame64");
+                if (pname.Length > 0)
+                {
+                    System.Windows.Forms.MessageBox.Show("Please close other instances of MAME and try again.");
+                } else
+                {
+                    if (File.Exists("mame64.exe"))
+                    {
+                        using (OpenFileDialog openFileDialog = new OpenFileDialog())
+                        {
+                            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                            openFileDialog.Filter = "CHD files (*.chd)|*.chd|All files (*.*)|*.*";
+                            openFileDialog.FilterIndex = 1;
+                            openFileDialog.RestoreDirectory = true;
+
+                            if (openFileDialog.ShowDialog() == DialogResult.OK)
+                            {
+                                game = openFileDialog.SafeFileName;
+                                status = timeToString + " elapsed";
+                                seconds = 0;
+                                process.StartInfo.FileName = "mame64.exe";
+                                process.StartInfo.Arguments = "cdimono1 -cdrom " + '"' + openFileDialog.FileName + '"';
+                                process.StartInfo.UseShellExecute = false;
+                                process.StartInfo.CreateNoWindow = true;
+                                openToolStripMenuItem.Enabled = false;
+                                process.Start();
+                            }
+                        }
+                    } else
+                    {
+                        System.Windows.Forms.MessageBox.Show("Please add mame64.exe to folder the launcher is in and try again.");
+                    }
+                }
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            time = TimeSpan.FromSeconds(seconds);
+
+            if (seconds > 3599)
+            {
+                timeToString = time.ToString(@"hh\:mm\:ss");
+            } else
+            {
+                timeToString = time.ToString(@"mm\:ss");
+            }
+
+            if (client == null) {}
+            else
+            {
+                if (disablingRPC == false)
+                {
+                    client.SetPresence(new RichPresence()
+                    {
+                        Details = game,
+                        State = status,
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = "rpc-icon"
+                        }
+                    });
+                }
+            }
+            Process[] pname = Process.GetProcessesByName("mame64");
+            if (pname.Length == 0)
+            {
+                openToolStripMenuItem.Enabled = true;
+                game = "Idle";
+                status = "Idle";
+            }
+            if (status != "Idle")
+            {
+                status = timeToString + " elapsed";
+            }
+        }
+
+        private void DiscordRichPresenceToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            if (discordRichPresenceToolStripMenuItem.Checked == false)
+            {
+                disablingRPC = true;
+                client.Dispose();
+            } else
+            {
+                disablingRPC = false;
+                RPCinit();
+            }
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("CDi-Launcher v1.0\nSupports CHD rom files\nMade by ArcadeGamer1929\nAvailable at: https://GitHub.com/ArcadeGamer1929/CDi-Launcher/releases" +
+            "\n\nThe Philips CD-i and MAME trademarks belong to their owners respectively");
+        }
+
+        private void MainWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process[] pname = Process.GetProcessesByName("mame64");
+            if (pname.Length == 0) {}
+            else
+            {
+                process.Kill();
+            }
+            Console.WriteLine("closing");
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
